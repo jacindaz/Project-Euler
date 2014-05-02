537c38351c5e78f03c601a3d33d8005485e0e380
Jacinda Zhong <jacindaz@gmail.com>
2014-05-02 15:51:35 -0400

    Find the smallest multiple - notes on how to solve
    Used the example number 2520 - this number is the smallest number
    divisible by each of the numbers 1-10. The problem is to find the
    smallest number divisible by 1-20.
    
    my notes indicate that with the combination of varaious number
    factoring, you can find the smallest number.

---
+++ problem5-smallestmultiple.rb
@@ -0,0 +1,19 @@
+=begin
+
+RANDOM THOUGHTS:
+  - in the example 2520, it is divisible by numbers 1-10
+  - from 1-10 remove all numbers that are divisible by each other
+  this includes: 1,2,3,4,5 (bc they are divisible by one of numbers 6-10)
+  - then take the factors of 6-10
+  - remove additional numbers, as long as the 2 factors can be found in
+     another combination.
+        for example: 6 = 2 x 3
+         BUT 8 = 4 x 2
+         so you can remove the 2 in 2x3=6
+  - keep whittling down, and you will have 2,3,3,4,5,7 left
+
+  Ta da! 2*3*3*4*5*7 = 2520
+
+=end
+
+
