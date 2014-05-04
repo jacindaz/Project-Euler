c2f849d04b23791a4301f5806f0a0e51abadfa4c
Jacinda Zhong <jacindaz@gmail.com>
2014-05-03 23:16:13 -0400

    Added arrayDivisible method, to return array with factors removed
    Input: a number that defines the range for an array
    Output: an array with numbers that are not factors of each other
    
    Steps:
     1) converts the range to an array
     2) checks all elements of the array to see if a number is a factor of another
     3) uses the factor? method to do this
     4) only checks if a smaller number is a factor of a larger number
     5) removes duplicates and sorts the array

--- problem5-smallestmultiple.rb
+++ problem5-smallestmultiple.rb
@@ -19,16 +19,44 @@ RANDOM THOUGHTS:
 def factor?(larger_num, smaller_num)
   return (larger_num % smaller_num) == 0
 end
-#puts factor?(20, 3)
-#puts factor?(20, 10)
+#puts factor?(20, 3)      #returns false
+#puts factor?(20, 10)     #returns true
 
+#returns an array of numbers that are not divisible by each other
+#given the highest number in a range (n), if n=10, range is 1..10
+def arrayDivisible(n)
+  factors_result = []
+  highest_num = n
+  numbers_range = *(2..n)
 
+  while highest_num > 1
+    #puts "inside while loop, highest_num: #{highest_num}"
+    numbers_range.each do |number|
+      #puts "inside each loop, highest_num: #{highest_num}, number: #{number}"
+      if factor?(highest_num, number) == false
+        next if highest_num <= number
+        puts "If statement, highest_num: #{highest_num}, number: #{number}"
+        factors_result << highest_num
+        numbers_range.delete(number)
+        #highest_num -= 1
+      end
+      puts "exit if statement"
+    end #end each loop
+    puts "exit each loop"
+    #return factors_result
+    highest_num -= 1
+  end #end while loop
 
-
-
-def smallestNumber(divisible_by)
-
-
-
+  factors_result = factors_result.uniq.sort
+  puts "This is the array: #{factors_result}"
 
 end
+
+arrayDivisible(10)
+
+
+#def smallestNumber(divisible_by)
+
+
+
+#end
