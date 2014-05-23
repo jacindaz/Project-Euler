6d2c76ab384ee14ed75223ece4f94646df0a23d0
Jacinda Zhong <jacindaz@gmail.com>
2014-05-23 13:26:25 -0400

    Added methods to remove factors and return primes
     - used the primes library
     - prime_division returns an array of arrays with the prime numbers of
       a given number
     - if all these numbers are multiplied, will get the smallest number
       that can be divided by all of a given range of numbers

--- problem5-smallestmultiple.rb
+++ problem5-smallestmultiple.rb
@@ -1,20 +1,6 @@
-=begin
+require 'pry'
+require 'prime'
 
-RANDOM THOUGHTS:
-  - in the example 2520, it is divisible by numbers 1-10
-  - from 1-10 remove all numbers that are divisible by each other
-  this includes: 1,2,3,4,5 (bc they are divisible by one of numbers 6-10)
-  - then take the factors of 6-10
-  - remove additional numbers, as long as the 2 factors can be found in
-     another combination.
-        for example: 6 = 2 x 3
-         BUT 8 = 4 x 2
-         so you can remove the 2 in 2x3=6
-  - keep whittling down, and you will have 2,3,3,4,5,7 left
-
-  Ta da! 2*3*3*4*5*7 = 2520
-
-=end
 
 def factor?(larger_num, smaller_num)
   return (larger_num % smaller_num) == 0
@@ -22,26 +8,53 @@ end
 #puts factor?(20, 3)      #returns false
 #puts factor?(20, 10)     #returns true
 
+#returns an array where n is the largest number
+#returns array where all numbers are not factors of each other
+def remove_factors(n)
+  numbers_range = *(2..n)
+  range_length = numbers_range.length
+  numbers_to_check = numbers_range[(range_length/2), range_length]
+  return numbers_to_check
+end
+#puts "#{remove_factors(10)}"
+
+
+def return_primes(n)
+  return n.prime_division
+end
+puts "#{return_primes(2520)}"
+
+
 #returns an array of numbers that are not divisible by each other
 #given the highest number in a range (n), if n=10, range is 1..10
 def arrayDivisible(n)
-  numbers_range = *(2..n)
-  not_factors = numbers_range
+  not_factors = []
+  numbers_to_check = remove_factors(n)
 
-  numbers_range.each do |smaller_number|
-    numbers_range.each do |bigger_number|
-      if factor?(bigger_number, smaller_number) == true
-        not_factors.delete(smaller_number)
+  index1 = 0
+  index2 = 1
+
+  puts "index1: #{index1}, index2: #{index2}, array: #{numbers_to_check}"
+  #binding.pry
+
+  while index1 <= range_length
+
+    while index2 <= range_length
+      if numbers_to_check[index2] % numbers_to_check[index1] != 0
+        puts "num1: #{numbers_to_check[index1]}, num2: #{numbers_to_check[index2]}, array: #{numbers_to_check}"
+        not_factors << numbers_to_check[index2]
+        puts "New array: #{not_factors}"
+        puts nil
       end
-      puts "smaller: #{smaller_number}, bigger: #{bigger_number}, array: #{not_factors}"
-      break
-    end #end bigger_number each loop
-  end #end smaller_number each loop
+      index2 += 1
+    end
+    index1 += 1
+  end
 
+  not_factors
 
-end #end arrayDivisible method
-
-arrayDivisible(20)
+end
+#arrayDivisible(20)
 
 
 #def smallestNumber(divisible_by)
@@ -49,4 +62,17 @@ arrayDivisible(20)
 
 
 #end
+
+
+
+
+
+
+
+
+
+
+
+
+
 
